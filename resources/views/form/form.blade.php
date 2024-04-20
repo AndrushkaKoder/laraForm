ea616cb25f9291c9488d62478dfe15da8ae4528b
Andrew <103961718+AndrushkaKoder@users.noreply.github.com>
2024-04-20 13:04:10 +0300

    Добавлен бутстрап, контроллер главной страницы и форма

---
+++ resources/views/form/form.blade.php
@@ -0,0 +1,9 @@
+<form action="#" method="post" class="d-flex justify-content-center align-items-center flex-column gap-3">
+	<label for="firstname">Имя</label>
+	<input type="text" id="firstname" name="firstname" class="form-control">
+	<label for="lastname">Фамилия</label>
+	<input type="text" id="lastname" name="lastname" class="form-control">
+	<label for="description">Описание</label>
+	<textarea rows="3" cols="3" id="description" name="description" class="form-control"></textarea>
+	<button class="btn btn-success">Submit form</button>
+</form>
